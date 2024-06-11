888c05eb1c97d22ae3c51fa8db270ab9e76974d5
Douglas Hirose <douglas.h@aluno.ifsp.edu.br>
2024-06-10 22:12:54 -0300

    mudanças miínimas

--- js/comentarios.js
+++ js/comentarios.js
@@ -1,4 +1,4 @@
-// Seleciona elementos
+
 var modal = document.getElementById("comentarioModal");
 var btn = document.getElementById("comenta");
 var span = document.getElementsByClassName("close")[0];
@@ -8,19 +8,16 @@ var cardsContainer = document.getElementById("cardsContainer2");
 var isEditing = false;
 var currentCard;
 
-// Função para abrir o modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Função para fechar o modal
 span.onclick = function() {
     modal.style.display = "none";
     form.reset();
     isEditing = false;
 }
 
-// Fecha o modal se o usuário clicar fora dele
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
@@ -29,11 +26,9 @@ window.onclick = function(event) {
     }
 }
 
-// Função para adicionar o card
 form.onsubmit = function(event) {
-    event.preventDefault(); // Evita o envio do formulário
+    event.preventDefault();
 
-    // Obtém os valores dos campos
     var nome = document.getElementById("nome").value;
     var comentario = document.getElementById("comentario").value;
 
@@ -42,7 +37,6 @@ form.onsubmit = function(event) {
         currentCard.querySelector("p").innerText = comentario;
         isEditing = false;
     } else {
-        // Cria um novo elemento div para o card
         var card = document.createElement("div");
         card.className = "card6";
         card.innerHTML = `
@@ -54,10 +48,9 @@ form.onsubmit = function(event) {
             </div>
         `;
 
-        // Adiciona o card ao container
         cardsContainer.appendChild(card);
 
-        // Adiciona eventos aos botões de editar e apagar
+
         card.querySelector(".edit-btn").onclick = function() {
             editCard(card);
         }
@@ -66,12 +59,10 @@ form.onsubmit = function(event) {
         }
     }
 
-    // Fecha o modal e limpa o formulário
     modal.style.display = "none";
     form.reset();
 }
 
-// Função para editar um card
 function editCard(card) {
     var nome = card.querySelector("h4").innerText;
     var comentario = card.querySelector("p").innerText;
@@ -84,7 +75,6 @@ function editCard(card) {
     currentCard = card;
 }
 
-// Função para apagar um card
 function deleteCard(card) {
     card.remove();
 }
